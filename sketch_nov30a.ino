0d89b4b89a0dc821a190ffc2bba3922f6d5dd57c
ZIbra-0 <zakariaouibra@gmail.com>
2024-12-03 12:17:32 -0500

    Add files via upload

---
+++ sketch_nov30a.ino
@@ -0,0 +1,39 @@
+void setup() {
+/* pinMode(X, OUTPUT);
+pinMode(X, OUTPUT);
+pinMode(X, OUTPUT);
+pinMode(X, OUTPUT);
+pinMode(X, OUTPUT);
+pinMode(X, OUTPUT);
+
+pinMode(X, INPUT);
+pinMode(X, INPUT);
+pinMode(X, INPUT);
+pinMode(X, INPUT);
+
+
+
+
+
+
+
+
+
+
+  int lvl1[3];
+  int lvl2[5];
+  int lvl3[8];
+  int lvl4[10];
+  int lvl5[12];
+  int lvl6[15];
+  int lvl7[20];
+*/
+int flash = rand() % 4 + 1;
+
+
+}
+
+void loop() {
+  int flash = rand() % 4 + 1;
+  Serial.println(flash);
+}
